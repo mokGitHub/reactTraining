1b5178a5affddc6a6ced46d7236accbe00075a3d
Sam.Mok <Sam.Mok@wnco.com>
2019-08-15 10:41:50 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App';
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
+/*
+ReactDOM.render(
+    <h1 className="test" style={{color: "red", paddingTop: 10}}>Hello John </h1>, document.getElementById("root"));
+*/
